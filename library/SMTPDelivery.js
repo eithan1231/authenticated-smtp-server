cf7d5f7388a39064e7d237d69bb7f85825ba0544
Silent <eithantuit10@gmail.com>
2019-05-27 15:48:17 +1000

    fixing reattempts

--- library/SMTPDelivery.js
+++ library/SMTPDelivery.js
@@ -10,50 +10,61 @@ module.exports = class SMTPDelivery
 	static send(sender, recipient, message)
 	{
 		return new Promise(async (resolve, reject) => {
-			const parsedRecipient = utilities.parseAddress(recipient);
-			if(!parsedRecipient) {
-				return resolve(false);
-			}
-
-			const recipientMXRecords = await utilities.resolveMXRecords(parsedRecipient.domain);
-			if(!recipientMXRecords) {
-				return resolve(false);
-			}
-
-			for(let recipientMXRecord of recipientMXRecords)
-			{
-				let dkim = null;
-				if(smtpConfig.dkim.enabled) {
-					dkim = {
-						domainName: smtpConfig.serverDomain,
-						keySelector: smtpConfig.dkim.keySelector,
-						privateKey: (await config.readConfig(smtpConfig.dkim.key)),
-						cacheDir: os.tmpdir(),
-					};
+			try {
+				const parsedRecipient = utilities.parseAddress(recipient);
+				if(!parsedRecipient) {
+					return resolve(false);
 				}
 
-				const transporter = nodemailer.createTransport({
-					secure: false,
-					port: 25,
-					host: recipientMXRecord.exchange,
-					name: smtpConfig.hostname,
-					dkim: dkim,
-					tls: {
-						rejectUnauthorized: false
+				const recipientMXRecords = await utilities.resolveMXRecords(parsedRecipient.domain);
+				if(!recipientMXRecords) {
+					return resolve(false);
+				}
+
+				for(let recipientMXRecord of recipientMXRecords)
+				{
+					try {
+						let dkim = null;
+						if(smtpConfig.dkim.enabled) {
+							dkim = {
+								domainName: smtpConfig.serverDomain,
+								keySelector: smtpConfig.dkim.keySelector,
+								privateKey: (await config.readConfig(smtpConfig.dkim.key)),
+								cacheDir: os.tmpdir(),
+							};
+						}
+
+						const transporter = nodemailer.createTransport({
+							secure: false,
+							port: 25,
+							host: recipientMXRecord.exchange,
+							name: smtpConfig.hostname,
+							dkim: dkim,
+							tls: {
+								rejectUnauthorized: false
+							}
+						});
+
+						// Need to set this on each recipient.
+						message.to = recipient;
+
+						if(await transporter.verify()) {
+							const info = await transporter.sendMail(message);
+							if(info.accepted.includes(recipient)) {
+								return resolve(true);
+							}
+						}
 					}
-				});
-
-				// Need to set this on each recipient.
-				message.to = recipient;
-
-				if(await transporter.verify()) {
-					const info = await transporter.sendMail(message);
-					if(info.accepted.includes(recipient)) {
-						return resolve(true);
+					catch(err) {
+						continue;
 					}
 				}
+
+				return resolve(false);
 			}
-			return resolve(false);
+			catch(err) {
+				return resolve(false);
+			}
 		});
 	}
 }
